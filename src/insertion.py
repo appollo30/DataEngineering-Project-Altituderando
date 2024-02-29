9c838ccd24b5525b00fcc9dc768252e5fe99ceb9
appollo30 <leo.lasnier@me.com>
2024-02-29 23:45:35 +0100

    ajout des services Elasticsearch et Kibana, connexion a Elasticsearch

--- src/insertion.py
+++ src/insertion.py
@@ -1,22 +1,42 @@
 import pandas as pd
 import pymongo
-import os
-
+from elasticsearch import Elasticsearch
+from os import environ
+from time import sleep
 
 print("fesses")
 
+#Variables d'environnement
 
-##Instanciation du client Mongo
-var = os.environ
-mongo_host = var.get("MONGO_HOST")
-mongo_port = var.get("MONGO_PORT")
+mongo_host = environ.get("MONGO_HOST")
+mongo_port = environ.get("MONGO_PORT")
+el_host = environ.get("ELASTICSEARCH_HOST")
+el_port = environ.get("ELASTICSEARCH_PORT")
 
-client = pymongo.MongoClient("localhost" if mongo_host == None else mongo_host,27017 if mongo_port == None else int(mongo_port))
-database = client['altituderando']
+
+#Instanciation du client Mongo
+
+mongo_client = pymongo.MongoClient("localhost" if mongo_host == None else mongo_host,27017 if mongo_port == None else int(mongo_port))
+print("Connexion à MongoDB réussie")
+database = mongo_client['altituderando']
 collection = database['pages']
 collection.delete_many({})
 
-##Traitement de la donnee depuis results.csv
+#Instanciation de la base Elasticsearch
+#Avec docker-compose, les services sont lances simultanement donc il arrive que la base elasticsearch soit en ligne après l'exécution de insertion.py, donc elle n'a pas le temps de se lancer qu'on essaie déjà de s'y connecter
+#On y remédie en faisant une boucle où on essaie de se connecter a repetition jusqu'a se connecter. On met le delai entre les connexions à 1 dixième de seconde.
+el_live = False
+retry_delay = 0.1
+
+while not el_live:
+    es_client = Elasticsearch(hosts=["localhost" if el_host == None else el_host])
+    el_live = es_client.ping()
+    sleep(retry_delay)
+print("Connexion à Elasticsearch réussie")
+
+
+
+#Traitement de la donnee depuis results.csv
 def extract_content(comments):
     return [comment['content'] for comment in comments]
 
@@ -29,9 +49,5 @@ data_dict = df.to_dict(orient="records")
 
 
 
-
+#Insertion dans la collection mongo
 collection.insert_many(data_dict)
-
-
-
-
