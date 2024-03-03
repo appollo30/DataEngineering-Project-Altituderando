2dbbbcfa6eed2da965977fa3d551578c51132169
appollo30 <leo.lasnier@me.com>
2024-03-04 00:51:18 +0100

    changements dans insertion.py, le code pour inserer dans mongo et dans elasticearch est divise en fonctions

--- src/insertion.py
+++ src/insertion.py
@@ -9,31 +9,59 @@ print("fesses")
 
 #Variables d'environnement
 
-mongo_host = environ.get("MONGO_HOST")
-mongo_port = environ.get("MONGO_PORT")
-el_host = environ.get("ELASTICSEARCH_HOST")
-el_port = environ.get("ELASTICSEARCH_PORT")
 
 
-#Instanciation du client Mongo
 
-mongo_client = pymongo.MongoClient("localhost" if mongo_host == None else mongo_host,27017 if mongo_port == None else int(mongo_port))
-print("Connexion à MongoDB réussie")
-database = mongo_client['altituderando']
-collection = database['pages']
-collection.delete_many({})
 
-#Instanciation de la base Elasticsearch
-#Avec docker-compose, les services sont lances simultanement donc il arrive que la base elasticsearch soit en ligne après l'exécution de insertion.py, donc elle n'a pas le temps de se lancer qu'on essaie déjà de s'y connecter
-#On y remédie en faisant une boucle où on essaie de se connecter a repetition jusqu'a se connecter. On met le delai entre les connexions à 1 dixième de seconde.
-el_live = False
-retry_delay = 0.1
+def insert_mongo(data):
+    #Variables d'environnement
+    mongo_host = environ.get("MONGO_HOST")
+    mongo_port = environ.get("MONGO_PORT")
+    #Instanciation du client Mongo
+    mongo_client = pymongo.MongoClient("localhost" if mongo_host == None else mongo_host,27017 if mongo_port == None else int(mongo_port))
+    print(mongo_host)
+    print(mongo_port)
+    print(mongo_client)
+    database = mongo_client['altituderando']
+    collection = database['pages']
+    #Insertion
+    data_dict_mongo = df.to_dict(orient="records")
+    collection.insert_many(data_dict_mongo)
+    return True
 
-while not el_live:
-    es_client = Elasticsearch(hosts=["localhost" if el_host == None else el_host])
-    el_live = es_client.ping()
-    sleep(retry_delay)
-print("Connexion à Elasticsearch réussie")
+
+def insert_es(data):
+    #Variables d'environnement
+    el_host = environ.get("ELASTICSEARCH_HOST")
+    el_port = environ.get("ELASTICSEARCH_PORT")
+    es_live = False
+    #On essaie pendant une minute de se connecter à Elasticsearch
+    retry_delay = 1
+    number_of_retries = 60
+
+    while not es_live and number_of_retries > 0:
+        #Instanciation du client Elasticsearch
+        #Avec docker-compose, les services sont lances simultanement donc il arrive que la base elasticsearch soit en ligne après l'exécution de insertion.py, donc elle n'a pas le temps de se lancer qu'on essaie déjà de s'y connecter
+        #On y remédie en faisant une boucle où on essaie de se connecter a repetition jusqu'a se connecter. On met le delai entre les connexions à une seconde.
+        es_client = Elasticsearch(hosts=["localhost" if el_host == None else el_host])
+        es_live = es_client.ping()
+        sleep(retry_delay)
+        number_of_retries += -1
+    if not es_live:
+        return False
+    #Traitement de la donnée
+    data_dict_es = df.fillna("").to_dict(orient="records")
+    def generate_data(documents):
+        for docu in documents:
+            yield {
+                "_index": "altituderando",
+                "_type": "page",
+                "_source": {k:v if v else None for k,v in docu.items()},
+            }
+    #Insertion dans Elasticsearch
+    bulk(es_client, generate_data(data_dict_es))
+    return True
+
 
 
 
@@ -47,21 +75,5 @@ df['comments'] = df['comments'].apply(extract_content)
 df['date'] = df['date'].astype(object).where(df['date'].notnull(), None)
 
 
-
-#Insertion dans la collection mongo
-data_dict_mongo = df.to_dict(orient="records")
-collection.insert_many(data_dict_mongo)
-print("Insertion dans mongo réussie")
-
-#Insertion dans Elasticsearch
-data_dict_elasticsearch = df.fillna("").to_dict(orient="records")
-def generate_data(documents):
-    for docu in documents:
-        yield {
-            "_index": "altituderando",
-            "_type": "page",
-            "_source": {k:v if v else None for k,v in docu.items()},
-        }
-
-bulk(es_client, generate_data(data_dict_elasticsearch))
-print("Insertion dans Elasticsearch réussie")
+print("Insertion dans Mongo: ", insert_mongo(df))
+print("Insertion dans Elasticsearch: ", insert_es(df))
